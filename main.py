ea6c95a0c35ee5001be364c92b6205b56c8e1a98
spider34 <spider344@proton.me>
2025-01-16 18:23:42 +0000

    count char

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from unittest import result
 def main():
     path = "books/frankenstein.txt"
     text = read_file(path)
-    print(count_words(text))
+    num_words = count_words(text)
+    print(count_char(text))
 
 def read_file(path_to_file):
     with open(path_to_file) as f:
@@ -9,5 +11,16 @@ def read_file(path_to_file):
 
 def count_words(text):
     return len(text.split())
+
+def count_char(text):
+    result = {}
+    for i in text:
+        i = i.lower()
+        if i in result:
+            result[i] += 1
+        elif i.isalpha():
+            result[i] = 1
+    return result
+
 
 main()
