52cfdd9c926319574a34b1e4eccb1680eb1062a1
spider34 <spider344@proton.me>
2025-01-16 18:15:33 +0000

    count words

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 def main():
     path = "books/frankenstein.txt"
-    print(read_file(path))
-
-
+    text = read_file(path)
+    print(count_words(text))
 
 def read_file(path_to_file):
     with open(path_to_file) as f:
         return f.read()
+
+def count_words(text):
+    return len(text.split())
 
 main()
